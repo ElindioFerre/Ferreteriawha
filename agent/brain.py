6ef146a201c1b7a3730f242a2251f8f83b298dfc
ElindioFerre <indiodirossipereira@gmail.com>
2026-04-02 16:36:49 -0300

    Update brain.py

--- agent/brain.py
+++ agent/brain.py
@@ -5,7 +5,7 @@ from agent.tools import buscar_precio
 async def generar_respuesta(mensaje_usuario, historial):
     api_key = os.getenv("GOOGLE_API_KEY")
     # MODELO ILIMITADO SEGÚN TU CONSOLA
-    model_name = "gemini-3.0-flash-live-preview" 
+   model_name = "gemini-3-flash-preview"
     url = f"https://generativelanguage.googleapis.com/v1beta/models/{model_name}:generateContent?key={api_key}"
     
     # 🔍 Búsqueda de precios antes de hablar
